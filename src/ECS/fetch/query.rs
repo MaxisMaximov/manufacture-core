3728801e99df0973664a2401284a0b4f90b53311
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-10-09 10:33:53 +0200

    Added Documentation to Query

--- src/ECS/fetch/query.rs
+++ src/ECS/fetch/query.rs
@@ -14,6 +14,11 @@ use super::{Fetch, FetchMut};
 /// It is implemented by default on `&` and `&mut` Component references, as well as Tuples up to 4 elements
 /// 
 /// The return type `Item` is typically the type the trait gets implemented on
+/// 
+/// `AccItem` is what gets returned when getting data immutably
+/// 
+/// `MutAccItem` is what gets returned when getting data mutably.  
+/// Read-only components simply return their `AccItem` when getting mutably, such as `&Component`
 pub trait QueryData{
     type Item<'b>;
     type AccItem<'b>;
@@ -21,17 +26,30 @@ pub trait QueryData{
 
     /// Fetch the data from the world
     fn fetch<'a>(World: &'a World) -> Self::Item<'a>;
+    /// Access given Entity's data immutably
     fn get<'a>(Fetched: &'a Self::Item<'a>, Index: &usize) -> Option<Self::AccItem<'a>>;
+    /// Access given Entity's data mutably
     fn get_mut<'a>(Fetched: &'a mut Self::Item<'a>, Index: &usize) -> Option<Self::MutAccItem<'a>>;
 }
 
 /// # World Query
 /// Struct that queries the World and fetches the specified `QueryData`, usually Components
+/// 
+/// To access the underlying Storages directly, use a dereference `*`
+/// 
+/// To get a specific Entity's set of components, use `get`, `get_mut`, and their Token variations.  
+/// Token variations of getters are preferred over normal getters
+/// 
+/// To iterate over all entities with all queried components, use `iter` and `iter_mut`
+/// 
+/// Query automatically validates Tokens in Getter functions, they can also be  
+/// manually validated via `validate_token`
 pub struct Query<'a, D: QueryData>{
     entities: &'a BTreeMap<usize, Entity>,
     data: D::Item<'a>
 }
 impl<'a, D: QueryData> Query<'a, D>{
+    /// Fetch `D`ata from the World
     pub fn fetch(World: &'a World) -> Self{
         Self{
             entities: World.get_entities(),
@@ -39,6 +57,13 @@ impl<'a, D: QueryData> Query<'a, D>{
         }
     }
 
+    /// Get a set of components for a given entity
+    /// 
+    /// It is generally discouraged to get components this way if you're looking for a specific Entity  
+    /// If you can, use `get_from_token` instead
+    /// 
+    /// Note that it returns `Some` only if the entity has *all* requested components,  
+    /// otherwise it returns `None`
     pub fn get(&'a self, Index: &usize) -> Option<D::AccItem<'a>>{
         if !self.entities.contains_key(Index){
             return None
@@ -46,6 +71,11 @@ impl<'a, D: QueryData> Query<'a, D>{
 
         D::get(&self.data, Index)
     }
+    /// Get a set of components for the Entity tracked by the Token.  
+    /// It automatically validates the given Token as well
+    /// 
+    /// Note that it returns `Some` only if the entity has *all* requested components,  
+    /// otherwise it returns `None`
     pub fn get_from_token(&'a self, Token: &mut entity::Token) -> Option<D::AccItem<'a>>{
         // We only accept valid Tokens
         if self.validate_token(Token){
@@ -55,6 +85,14 @@ impl<'a, D: QueryData> Query<'a, D>{
         }
 
     }
+
+    /// Get a mutable set of components for a given entity
+    /// 
+    /// It is generally discouraged to get components this way if you're looking for a specific Entity.  
+    /// If you can, use `get_from_token_mut` instead
+    /// 
+    /// Note that it returns `Some` only if the entity has *all* requested components,  
+    /// otherwise it returns `None`
     pub fn get_mut(&'a mut self, Index: &usize) -> Option<D::MutAccItem<'a>>{
         if !self.entities.contains_key(Index){
             return None
@@ -62,6 +100,11 @@ impl<'a, D: QueryData> Query<'a, D>{
         
         D::get_mut(&mut self.data, Index)
     }
+    /// Get a mutable set of components for the Entity tracked by the Token.  
+    /// It automatically validates the given Token as well
+    /// 
+    /// Note that it returns `Some` only if the entity has *all* requested components,  
+    /// otherwise it returns `None`
     pub fn get_from_token_mut(&'a mut self, Token: &mut entity::Token) -> Option<D::MutAccItem<'a>>{
         // We only accept valid Tokens
         if self.validate_token(Token){
@@ -70,13 +113,19 @@ impl<'a, D: QueryData> Query<'a, D>{
             None
         }
     }
+
+    /// Iterate over all matching entities immutably  
+    /// 
+    /// Entities that don't have at least one matching component will not be iterated over
     pub fn iter(&'a self) -> Iter<'a, D>{
         Iter{
             data: &self.data,
             ent_iter: self.entities.keys(),
         }
     }
-
+    /// Iterate over all matching entities mutably  
+    /// 
+    /// Entities that don't have at least one matching component will not be iterated over
     pub fn iter_mut(&'a mut self) -> IterMut<'a, D>{
         IterMut{
             data: &mut self.data,
@@ -84,6 +133,9 @@ impl<'a, D: QueryData> Query<'a, D>{
         }
     }
 
+    /// Validate an Entity Token  
+    /// 
+    /// Updates Token's `valid` flag and returns boolean whether it's still valid or not
     pub fn validate_token(&self, Token: &mut entity::Token) -> bool{
         // If a Token is invalid, it can never again be valid
         if !Token.valid(){
@@ -115,6 +167,8 @@ impl<'a, D: QueryData> DerefMut for Query<'a, D>{
 ///////////////////////////////////////////////////////////////////////////////
 
 use std::collections::btree_map::Keys;
+/// # Query Iterator
+/// Iterates over entities that have all matching components of `D`ata immutably
 pub struct Iter<'a, D: QueryData>{
     data: &'a D::Item<'a>,
     ent_iter: Keys<'a, usize, Entity>
@@ -133,6 +187,8 @@ impl<'a, D: QueryData> Iterator for Iter<'a, D>{
     }
 }
 
+/// # Mutable Query Iterator
+/// Iterates over entities that have all matching components of `D`ata mutably
 pub struct IterMut<'a, D: QueryData>{
     data: &'a mut D::Item<'a>,
     ent_iter: Keys<'a, usize, Entity>
@@ -152,8 +208,8 @@ impl<'a, D: QueryData> Iterator for IterMut<'a, D>{
                     // post's last comment suggests for a whole different problem
 
                     // I PRESUME:
-                    // 1. We -grade (up or down??) `self.data`, which is a mutable 
-                    //    reference to Query's `data` field, into a mutable *pointer*
+                    // 1. We -grade (up or down??) `self.data` - which is a mutable 
+                    //    reference to Query's `data` field - into a mutable *pointer*
                     // 2. We dereference that pointer to get to the original
                     //    data, getting *it's* lifetime now instead of Query's
                     // 3. We then pass that direct original data as a mutable reference into the Getter
@@ -217,7 +273,6 @@ impl<C: Component> QueryData for &mut C{
 // Tuples
 ///////////////////////////////////////////////////////////////////////////////
 
-#[macro_export]
 macro_rules! query_impl {
     ($($x:ident), *) => {
         impl<$($x: QueryData), *> QueryData for ($($x), *){
