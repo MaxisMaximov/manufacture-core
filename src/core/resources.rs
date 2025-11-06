0e2097e517d042ecb029b745343e9b083adb46c0
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-11-06 09:17:14 +0100

    Added `DeltaT` resource

--- src/core/resources.rs
+++ src/core/resources.rs
@@ -1,3 +1,32 @@
 use super::ECS;
 
 use ECS::resource::Resource;
+
+/// # Delta Time Resources
+/// Tracks milliseconds elapsed since last frame and last Logic run. Use provided `delta_frame` and `delta_logic` methods to get the time
+/// 
+/// Only the Dispatcher can modify the inner values, fetching this mutably does not do anything
+pub struct DeltaT{
+    delta_frame: u128,
+    delta_logic: u128
+}
+impl DeltaT{
+    /// Get milliseconds elapsed since last frame
+    pub fn delta_frame(&self) -> u128{
+        self.delta_frame
+    }
+    /// Get milliseconds elapsed since last time Logic ran
+    pub fn delta_logic(&self) -> u128{
+        self.delta_logic
+    }
+}
+impl Resource for DeltaT{
+    const ID: &'static str = "DeltaT";
+
+    fn new() -> Self {
+        Self{
+            delta_frame: 0,
+            delta_logic: 0
+        }
+    }
+}
