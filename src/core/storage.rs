dbc98fe7810de3bf507127362a086e432f0dd231
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-11-07 10:36:27 +0100

    Added DenseVecStorage back

--- src/core/storage.rs
+++ src/core/storage.rs
@@ -99,3 +99,53 @@ impl<C: Component> Storage<C> for BTreeMapStorage<C>{
         self.inner.get_mut(Index)
     }
 }
+
+/// # DenseVecStorage
+/// A Vec Storage that uses a Hashmap as fast index proxy
+/// 
+/// Best of HashMap and Vec Storages, with the density of Vec and fast access time of HashMap
+/// 
+/// It's generally recommended to use this for sparsely populated, but heavy components
+pub struct DenseVecStorage<C: Component>{
+    proxy: HashMap<usize, usize>,
+    inner: Vec<(usize, C)>
+}
+impl<C: Component> Storage<C> for DenseVecStorage<C>{
+    fn new() -> Self {
+        Self{
+            proxy: HashMap::new(),
+            inner: Vec::new(),
+        }
+    }
+
+    fn insert(&mut self, Index: usize, Comp: C) {
+        if self.proxy.contains_key(&Index){
+            return
+        }
+
+        self.proxy.insert(Index, self.inner.len());
+        self.inner.push((Index, Comp));
+    }
+    fn remove(&mut self, Index: &usize) {
+        if let Some(inner_index) = self.proxy.remove(Index){
+            self.inner.swap_remove(inner_index);
+            // It was the only element in the Storage, we return early
+            if self.inner.is_empty(){
+                return
+            }
+            // Now we update the proxy map value that linked to the last element
+            let to_update = self.inner[*Index].0;
+            *self.proxy.get_mut(&to_update).unwrap() = *Index;
+        }
+    }
+
+    fn get(&self, Index: &usize) -> Option<&C> {
+        let index = self.proxy.get(Index)?;
+        // Bit of a mess here
+        self.inner.get(*index).map(|(_, comp)| comp)
+    }
+    fn get_mut(&mut self, Index: &usize) -> Option<&mut C> {
+        let index = self.proxy.get(Index)?;
+        self.inner.get_mut(*index).map(|(_, comp)| comp)
+    }
+}
