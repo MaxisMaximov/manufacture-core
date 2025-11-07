7bad5fcb86c287d159217d57e04c777765a879c9
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-11-07 11:23:55 +0100

    Impld Component for components

--- src/core/comp.rs
+++ src/core/comp.rs
@@ -10,15 +10,30 @@ pub struct Vector2{
     pub y: f32,
     pub z_index: u8,
 }
+impl Component for Vector2{
+    type STORAGE = BTreeMapStorage<Self>;
+
+    const ID: &'static str = "Vector2";
+}
 
 pub struct Vector3{
     pub x: f32,
     pub y: f32,
     pub z: f32
 }
+impl Component for Vector3{
+    type STORAGE = BTreeMapStorage<Self>;
+
+    const ID: &'static str = "Vector3";
+}
 
 pub struct Tags{
     pub inner: HashSet<&'static str>
+}
+impl Component for Tags{
+    type STORAGE = HashMapStorage<Self>;
+
+    const ID: &'static str = "Tags";
 }
 
 pub struct CMDSprite{
@@ -26,8 +41,18 @@ pub struct CMDSprite{
     pub size_y: u8,
     pub data: Vec<(char, (u8, u8, u8), (u8, u8, u8))> // Symbol, Foreground RGB, Background RGB
 }
+impl Component for CMDSprite{
+    type STORAGE = HashMapStorage<Self>;
+
+    const ID: &'static str = "CMDSprite";
+}
 
 pub struct PlayerController{
     pub pid: u16,
     pub active: bool,
+}
+impl Component for PlayerController{
+    type STORAGE = VecStorage<Self>;
+
+    const ID: &'static str = "PlayerController";
 }
