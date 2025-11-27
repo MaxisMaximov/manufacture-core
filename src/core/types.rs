bded896e2149ec7639045f0f5e2d5b362f6ee9c4
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-11-27 10:35:28 +0100

    Added a bunch of basic functions to Vector3 + documentation

--- src/core/types.rs
+++ src/core/types.rs
@@ -89,6 +89,9 @@ impl Vector2{
         (self.dot(other) / (self.magnitude() * other.magnitude())).acos()
     }
     /// Returns the length of `self`
+    /// 
+    /// ## WARNING
+    /// Because this uses Pythagorean Theorem to calculate, a Vector too big may cause an overflow, and subsequently a crash
     pub fn magnitude(self) -> f32{
         (self.x.powi(2) + self.y.powi(2)).sqrt()
     }
@@ -165,11 +168,66 @@ impl DivAssign<f32> for Vector2 {
 }
 
 /// A simple 3D coordinate type
+#[derive(Clone, Copy)]
 pub struct Vector3{
     pub x: f32,
     pub y: f32,
     pub z: f32
 }
+impl Vector3{
+    /// Create a new 2D Vector
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self{x, y, z}
+    }
+    /// Returns the dot product of `self` and `other`
+    /// 
+    /// Simpler explanation: How similar the two Vectors are
+    /// - Sign defines direction, positive means same direction, negative means opposite direction
+    /// - Value defines how similar the magnitude is
+    pub fn dot(self, other: Self) -> f32{
+        (self.x * other.x) + (self.y * other.y) + (self.z * other.z)
+    }
+    /// Returns a Vector that's a projection of `self` onto `other`
+    /// 
+    /// Simpler explanation: It flattens `self` onto `other`
+    pub fn project(self, other: Self) -> Self{
+        let scalar = self.dot(other)/other.magnitude().powi(2);
+        self * scalar
+    }
+    /// Returns the cross product Vector of `self` and `other`
+    /// 
+    /// Simpler explanation: Returns a Vector perpendicular to `self` and `other`, the magnitude of it is inverse of dot product: it defines how *different* the two Vectors are
+    pub fn cross(self, other: Self) -> Self{
+        Self{
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+    /// Returns a reflection of `self` around `other`
+    pub fn reflect(self, other: Self) -> Self{
+        self.project(other) * 2.0 - self
+    }
+    /// Returns distance between endpoint of `self` and `other`
+    pub fn distance(self, other: Self) -> f32{
+        (self - other).magnitude()
+    }
+    /// Returns self normalized to magnitude of 1
+    pub fn normalize(self) -> Self{
+        self / self.magnitude()
+    }
+    /// Returns the angle between `self` and `other` in Radians
+    pub fn angle_between(self, other: Self) -> f32{
+        (self.dot(other) / (self.magnitude() * other.magnitude())).acos()
+    }
+    /// Returns the length of `self`
+    /// 
+    /// ## WARNING
+    /// Because this uses Pythagorean Theorem to calculate, a Vector too big may cause an overflow, and subsequently a crash
+    pub fn magnitude(self) -> f32{
+        (self.x.powi(2) + self.y.powi(2) + self.x.powi(2)).sqrt()
+    }
+}   
 impl std::fmt::Display for Vector3{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!("({}, {})", self.x, self.y))
