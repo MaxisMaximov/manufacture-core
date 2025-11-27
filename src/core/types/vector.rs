3719f6460e17a5806849a09e8d9617601238715e
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-11-27 10:38:06 +0100

    Split off Vectors into their own file

--- src/core/types/vector.rs
+++ src/core/types/vector.rs
@@ -1,41 +1,3 @@
-use std::marker::PhantomData;
-
-use super::*;
-use crate::ECS::fetch::Fetch;
-
-/// # Query Filter: With
-/// Only allows Entities that have the specified component to pass through
-/// 
-/// There is no need to include it in the filters if you fetch the component,  
-/// Query automatically checks whether the requested components exist for an Entity
-pub struct With<C: Component>(PhantomData<C>);
-impl<C: Component> QueryFilter for With<C>{
-    type Item<'b> = Fetch<'b, C>;
-
-    fn fetch<'a>(World: &'a World) -> Self::Item<'a> {
-        World.fetch::<C>()
-    }
-
-    fn filter<'a>(Fetched: &'a Self::Item<'a>, Index: &usize) -> bool {
-        Fetched.get(Index).is_some()
-    }
-}
-
-/// # Query Filter: Without
-/// Only allows Entities without the specified component to pass through
-pub struct Without<C: Component>(PhantomData<C>);
-impl<C: Component> QueryFilter for Without<C>{
-    type Item<'b> = Fetch<'b, C>;
-
-    fn fetch<'a>(World: &'a World) -> Self::Item<'a> {
-        World.fetch::<C>()
-    }
-
-    fn filter<'a>(Fetched: &'a Self::Item<'a>, Index: &usize) -> bool {
-        Fetched.get(Index).is_none()
-    }
-}
-
 use std::ops::{
     Add,
     AddAssign,
@@ -230,7 +192,7 @@ impl Vector3{
 }   
 impl std::fmt::Display for Vector3{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("({}, {})", self.x, self.y))
+        f.write_fmt(format_args!("({}, {}, {})", self.x, self.y, self.z))
     }
 }
 impl Add for Vector3{
