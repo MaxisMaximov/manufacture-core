51b6c2db754e6a4b3d4a1406a68e9473f7a6edcb
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-11-28 13:58:29 +0100

    Added Spawn/Despawn commands

--- src/core/commands.rs
+++ src/core/commands.rs
@@ -1,1 +1,28 @@
 use super::*;
+
+use events::{EntitySpawned, EntityDespawned};
+
+pub struct Spawn;
+impl Command for Spawn{
+    fn execute(&mut self, World: &mut World) {
+        let token = World.spawn().get_token();
+        World.get_event_writer::<EntitySpawned>().send(EntitySpawned(token));
+    }
+}
+
+pub struct DespawnID(pub usize);
+impl Command for DespawnID{
+    fn execute(&mut self, World: &mut World) {
+        World.despawn(self.0);
+        World.get_event_writer::<EntityDespawned>().send(EntityDespawned(self.0));
+    }
+}
+
+pub struct DespawnToken(pub Token);
+impl Command for DespawnToken{
+    fn execute(&mut self, World: &mut World) {
+        if World.despawn_with_token(self.0){
+            World.get_event_writer::<EntityDespawned>().send(EntityDespawned(self.0.id()));
+        }
+    }
+}
