f6023dd85b5bb5e8b51fdf6a812bb42cd2521576
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-11-28 13:58:00 +0100

    Added App Exit system event

--- src/core/events.rs
+++ src/core/events.rs
@@ -13,3 +13,13 @@ pub struct EntityDespawned(pub usize);
 impl Event for EntityDespawned{
     const ID: &'static str = "EntityDespawned";
 }
+
+/// **System Level Event**
+/// 
+/// Announces to the Dispatcher that the app is to be shut down
+/// 
+/// TODO: Error Codes themselves
+pub struct ExitApp(pub i32);
+impl Event for ExitApp{
+    const ID: &'static str = "_APP_EXIT";
+}
