7dc8e6c3bbb0f7432c456c72c497665b355d8e11
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-12-15 14:14:49 +0100

    Added CMD Input Handler system

--- src/core/systems.rs
+++ src/core/systems.rs
@@ -3,3 +3,25 @@ use comp::*;
 use resources::*;
 use events::*;
 use commands::*;
+pub struct CMDInputHandler;
+impl System for CMDInputHandler{
+    type Data = &'static mut CMDInput;
+
+    const ID: &'static str = "CMDInput";
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn execute(&mut self, mut Data: Request<'_, Self::Data>) {
+        use crossterm::event::{Event, read, poll};
+        if poll(std::time::Duration::from_millis(0)).unwrap(){
+            if let Event::Key(key) = read().unwrap(){
+                // Triple Deref, whoops
+                ***Data = key
+            }
+        }else{
+            ***Data = KeyEvent::new(KeyCode::Null, KeyModifiers::NONE)
+        }
+    }
+}
